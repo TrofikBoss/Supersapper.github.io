6a1b6e0c82b7bc690e35d2721e7648096aa986f3
TrofikBoss <118187409+TrofikBoss@users.noreply.github.com>
2023-06-29 19:25:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let map = [];
 let minelocate = [];
 let gameover = false;
 let gameplay = false;
-let firststart = true;
 let difficulty = "easy";
+let theme = "orange";
+
+function changeTheme(themeid) {
+  theme = themeid;
+  document.querySelector("link.theme").href = `theme/${theme}.css`;
+}
 
 function GenArea(maxx, maxy, propmine, diff) {
   gameover = false;
@@ -250,12 +255,6 @@ function MenuBack() {
   })
 }
 function PlayGo() {
-  /*if (!firststart) {
-    bridge.send('VKWebAppShowBannerAd', {
-      banner_location: 'bottom'
-    });
-  }*/
-  //firststart = false;
   document.querySelector(".menu").classList.add("hidden");
   document.querySelectorAll("#game-easy, #game-normal, #game-hard, #menu-back").forEach(function(one123) {
     one123.classList.add("hidden");
